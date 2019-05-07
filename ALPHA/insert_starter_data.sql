b6fe8c557dbcb2c52c5de3211a54dcd1fcd7bfc7
Emma Lurie <elurie@wellesley.edu>
2019-05-07 12:37:15 +0000

    removed comma, changed date format

--- ALPHA/insert_starter_data.sql
+++ ALPHA/insert_starter_data.sql
@@ -7,12 +7,13 @@ fields terminated by '\t'
 lines terminated by '\n'
 IGNORE 1 ROWS;
 
-load data local infile 'tips.tsv' 
+load data local infile 'tips_2.tsv' 
 into table tips
 fields terminated by '\t'
 lines terminated by '\n'
-IGNORE 1 ROWS;
+IGNORE 1 ROWS
 (@var_datePosted)
-SET datePosted = STR_TO_DATE(@var_col1,'%c/%e/%Y %h:%i %p');
+SET datePosted = STR_TO_DATE(@var_datePosted,"%Y-%m-%d");
 -- date posted isn't working 
+
 
