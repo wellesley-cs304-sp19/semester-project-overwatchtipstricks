dc0c80bb21e922afa7d6a807f4d6c5158f28e5ff
Emma Lurie <elurie@wellesley.edu>
2019-05-17 20:56:05 +0000

    addOptions comments

--- BETA/static/addOptions.js
+++ BETA/static/addOptions.js
@@ -1,7 +1,10 @@
+/*on document load, populate the hero, maps, and difficulty dropdowns. 
+The text of each option item  as well as the "value" is the hero, map,
+or difficulty name. */
 
-var select = document.getElementById("heroes"); 
 
-var h = ['All','Ashe','Bastion','Doomfist','Genji','Hanzo','Junkrat','McCree',
+//staic list of all heroes, maps, and difficulty in our DB
+var heroes = ['All','Ashe','Bastion','Doomfist','Genji','Hanzo','Junkrat','McCree',
     'Mei','Pharah','Reaper','Soldier:76','Sombra','Symmetra','Torbjorn',
     'Tracer','Widowmaker','D.Va','Orisa','Reinhardt','Roadhog','Winston',
     'Wrecking Ball','Zarya','Ana','Baptiste','Bridgette','Lúcio','Mercy',
@@ -15,8 +18,12 @@ var maps = ['All','Hanamura','Horizon Lunar Colony','Paris','Temple of Anubis',
 var difficulty = ['All', 'Beginner', 'Intermediate', 'Advanced', 'Expert'];
 
 
+//on document ready, populate the menu bars
+/*when posting tips, there are two different menu bars to populate, 
+so fill in both ids (e.g. heroes and tipHero)*/
+
  $( document ).ready(function() {
- $.each(h, function(val, text) {
+ $.each(heroes, function(val, text) {
             $('#heroes').append( $('<option></option>').val(text).html(text) );
             $('#tipHero').append( $('<option></option>').val(text).html(text) );
             }); 
